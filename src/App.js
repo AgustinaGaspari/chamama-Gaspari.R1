6599ebee2434ffcb168c7d657c9a8bc845a8a7ec
Giuliana Velasco <velascogiuliana@gmail.com>
2022-07-11 23:43:41 -0300

    creacion de la app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = 'Chamama'
+  const name = 'CHAMAMA'
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. {name}
+          {name}
         </p>
         <a
           className="App-link"
