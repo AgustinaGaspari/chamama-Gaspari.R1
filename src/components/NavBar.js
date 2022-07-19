2b3638c2de1c5da32e90e79474c972dc3f0f091e
Agus Gaspari <agusgaspari05@gmail.com>
2022-07-19 00:04:47 -0300

    menu general con bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,33 @@
 
-/*import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function ContainerOutsideExample(){
+
+function BasicExample() {
   return (
-    <Container>
-      <Navbar expand="lg" variant="light" bg="light">
-        <Container>
-          <Navbar.Brand href="#">PRODUCTOS</Navbar.Brand>
-        </Container>
-      </Navbar>
-    </Container>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand className="tienda" href="#home">TIENDA CHAMAMA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">HOME</Nav.Link>
+            <Nav.Link href="#link">CONOCENOS</Nav.Link>
+            <NavDropdown title="PRODUCTOS" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">BOLSOS</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2"> BILLETERAS</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">TABAQUERAS</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">BANDOLERAS</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default ContainerOutsideExample;
-*/
+export default BasicExample;
+
 
